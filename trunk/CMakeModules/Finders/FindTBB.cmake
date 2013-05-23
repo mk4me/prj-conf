66f2f30109decf059f6b6a300a4716a345f61c77
mati_egon <mati_egon@0ea9e9b8-efba-4331-8c9d-1dd0d45bf26d>
2013-05-23 10:41:45 +0000

    Fix instalowania translacji Fixy dla szukania bibliotek pod linuxem (pewnie jeszcze nie wszystkie - tyle się udało z Adamem Gudysiem ustalić)
    git-svn-id: https://localhost:8443/svn/prj-conf@57 0ea9e9b8-efba-4331-8c9d-1dd0d45bf26d

--- trunk/CMakeModules/Finders/FindTBB.cmake
+++ trunk/CMakeModules/Finders/FindTBB.cmake
@@ -2,14 +2,8 @@
 FIND_INIT(TBB tbb)
 
 # szukanie
-if (WIN32)
-	FIND_SHARED(TBB_CORE "<lib,?>tbb<_debug,?>" "<lib,?>tbb<_debug,?>")
-	FIND_SHARED(TBB_MALLOC "<lib,?>tbbmalloc<_debug,?>" "<lib,?>tbbmalloc<_debug,?>")
-	FIND_SHARED(TBB_MALLOC_PROXY "<lib,?>tbbmalloc_proxy<_debug,?>" "<lib,?>tbbmalloc_proxy<_debug,?>")
-else()
-	FIND_GLOB(TBB_CORE "libtbb.so.*" "libtbb.so.*")
-	FIND_GLOB(TBB_MALLOC "libtbbmalloc.so.*" "libtbbmalloc.so.*")
-	FIND_GLOB(TBB_MALLOC_PROXY "libtbbmalloc_proxy.so.*" "libtbbmalloc_proxy.so.*")
-endif()
+FIND_SHARED(TBB_CORE "<lib,?>tbb<_debug,?>" "<lib,?>tbb<_debug,?>")
+FIND_SHARED(TBB_MALLOC "<lib,?>tbbmalloc<_debug,?>" "<lib,?>tbbmalloc<_debug,?>")
+FIND_SHARED(TBB_MALLOC_PROXY "<lib,?>tbbmalloc_proxy<_debug,?>" "<lib,?>tbbmalloc_proxy<_debug,?>")
 # koniec
 FIND_FINISH(TBB)
