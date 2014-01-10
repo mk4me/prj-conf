88a5fcc4950bc14934cfda5761fae477e1913679
mati_egon <mati_egon@0ea9e9b8-efba-4331-8c9d-1dd0d45bf26d>
2014-01-10 11:19:59 +0000

    Dodanie finderow dla bibliotek obsługugujących kostium z IMU - biblioteki dostarczane są przez konstruktorow kostiumu
    git-svn-id: https://localhost:8443/svn/prj-conf@90 0ea9e9b8-efba-4331-8c9d-1dd0d45bf26d

---
+++ trunk/CMakeModules/Finders/FindBIC.cmake
@@ -0,0 +1,7 @@
+FIND_INIT(BIC bic)
+
+# szukanie
+FIND_SHARED(BIC "BIC" "BIC")
+
+# skopiowanie
+FIND_FINISH(BIC)
---
+++ trunk/CMakeModules/Finders/FindKDAR.cmake
@@ -0,0 +1,7 @@
+FIND_INIT(KDAR kdar)
+
+# szukanie
+FIND_SHARED(KDAR "KDAR" "KDAR")
+
+# skopiowanie
+FIND_FINISH(KDAR)
