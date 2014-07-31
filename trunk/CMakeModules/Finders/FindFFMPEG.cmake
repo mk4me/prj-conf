8150821b5d5ee5d8f696c14f178158a74d66bc6d
mati_egon <mati_egon@0ea9e9b8-efba-4331-8c9d-1dd0d45bf26d>
2014-07-31 11:26:12 +0000

    Dodanie wyszukiwania biblioteki swresample + poprawka zmiennej dla FIND_DEPENDENCIES
    git-svn-id: https://localhost:8443/svn/prj-conf@114 0ea9e9b8-efba-4331-8c9d-1dd0d45bf26d

--- trunk/CMakeModules/Finders/FindFFMPEG.cmake
+++ trunk/CMakeModules/Finders/FindFFMPEG.cmake
@@ -2,13 +2,15 @@
 FIND_INIT(FFMPEG ffmpeg)
 
 FIND_SHARED(FFMPEG_LIBAVCODEC "avcodec<-55,?>" "avcodec-55")
-FIND_SHARED(FFMPEG_LIBAVFORMAT "avformat<-55,?>" "avformat-55")
 FIND_SHARED(FFMPEG_LIBAVDEVICE "avdevice<-55,?>" "avdevice-55")
+FIND_SHARED(FFMPEG_LIBAVFILTER "avfilter<-4,?>" "avfilter-4")
+FIND_SHARED(FFMPEG_LIBAVFORMAT "avformat<-55,?>" "avformat-55")
 FIND_SHARED(FFMPEG_LIBAVUTIL "avutil<-52,?>" "avutil-52")
+FIND_SHARED(FFMPEG_LIBSWRESAMPLE "swresample<-0,?>" "swresample-0")
 FIND_SHARED(FFMPEG_LIBSWSCALE "swscale<-2,?>" "swscale-2")
 
 if(UNIX)
-	FIND_DEPENDENCIES(CURL "OPENSSL;FREETYPE")
+	FIND_DEPENDENCIES(FFMPEG "OPENSSL;FREETYPE")
 endif()
 
 # skopiowanie
