1191fae2c191b16b10d8cba6f6fb559d4e372d3b
wojciech.kniec <wojciech.kniec@0ea9e9b8-efba-4331-8c9d-1dd0d45bf26d>
2016-02-12 10:25:42 +0000

    poprawka linux
    git-svn-id: https://localhost:8443/svn/prj-conf@160 0ea9e9b8-efba-4331-8c9d-1dd0d45bf26d

--- trunk/CMakeModules/Finders/FindCMINPACK.cmake
+++ trunk/CMakeModules/Finders/FindCMINPACK.cmake
@@ -1,4 +1,11 @@
 # przygotowanie do szukania
 FIND_INIT(CMINPACK cminpack)
 FIND_STATIC_EXT(CMINPACK "cminpack" "cminpack")
+
+if (WIN32) 	
+	FIND_STATIC_EXT(CMINPACK "cminpack" "cminpack")
+elseif (UNIX)
+	FIND_SHARED(CMINPACK "cminpack" "cminpack")
+endif()
+
 FIND_FINISH(CMINPACK)
