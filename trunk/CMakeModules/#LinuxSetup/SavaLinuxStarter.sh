743603c868ea1f58c1c10c1c2343396d28324a9c
wojciech.kniec <wojciech.kniec@0ea9e9b8-efba-4331-8c9d-1dd0d45bf26d>
2016-10-08 10:35:40 +0000

    starter dla SAVA
    git-svn-id: https://localhost:8443/svn/prj-conf@166 0ea9e9b8-efba-4331-8c9d-1dd0d45bf26d

---
+++ trunk/CMakeModules/#LinuxSetup/SavaLinuxStarter.sh
@@ -0,0 +1,48 @@
+# Skrypt instaluje wszystkie potrzebne rzeczy do skompilowania i uruchomienia sava-linux na Linuxie
+# należy skopiować skrypty SavaLinuxStarter.sh i StarterApps.sh do folderu domowego, nadac prawa do uruchomienia i uruchomić
+# chmod +x SavaLinuxStarter.sh
+# ./SavaLinuxStarter
+# Skrypt zapyta o hasło roota w celu instalacji aplikacji
+#
+#	Wykonywane operacje:
+#	1. Pobranie i zainstalowanie potrzebnych aplikacji (StarterApps.sh)
+#	2. Utworzenie struktury katalogów
+#	3. Ściągnięcie źródeł z svn
+#	4. Ściągnięcie najnowszych bibliotek
+#	5. Rozpakowanie ich i utworzenie dowiązań symbolicznych w trunk-u SAVA
+# Instalacja przebiega bez ingerencji uzytkownika, nalezy tylko wypelnic zmienne nizej
+
+assembla_user=""
+assembla_pass=""
+
+
+ftp_adress="83.230.112.91/lib/SAVA/"
+ftp_user="admin"
+ftp_pass="S0snoW1e!c"
+
+sudo sh ./StarterApps.sh
+
+mkdir ~/programming
+mkdir ~/programming/Work
+mkdir ~/programming/Work/SAVA
+mkdir ~/programming/Work/SAVA/_out
+mkdir ~/programming/Work/SAVA/SourceCode
+mkdir ~/programming/Work/SAVA/Libs
+
+if [ -z "$assembla_user"] || [ -z "$assembla_pass" ]; then 
+svn checkout https://subversion.assembla.com/svn/sava-linux/ ~/programming/Work/SAVA/SourceCode 
+else  
+svn checkout --non-interactive --no-auth-cache --username $assembla_user --password $assembla_pass https://subversion.assembla.com/svn/sava-linux/ ~/programming/Work/SAVA/SourceCode 
+fi
+
+file=`ncftpls -m ftp://$ftp_user:$ftp_pass@$ftp_adress  | grep linux64 | sort -k1 | awk '{ print $2 }' | tail -n1`
+echo "~/programming/Work/MDE/Libs/$file"
+ncftpget ftp://$ftp_user:$ftp_pass@$ftp_adress$file 
+mv $file ~/programming/Work/SAVA/Libs/$file
+folder="${file%.*}"
+7z x ~/programming/Work/SAVA/Libs/$file -o./programming/Work/SAVA/Libs/$folder/
+
+ln -s ~/programming/Work/SAVA/Libs/$folder/include ~/programming/Work/SAVA/SourceCode/trunk/include
+ln -s ~/programming/Work/SAVA/Libs/$folder/lib     ~/programming/Work/SAVA/SourceCode/trunk/lib
+
+#sh ./MdeCMake.sh
--- trunk/CMakeModules/#LinuxSetup/StarterApps.sh
+++ trunk/CMakeModules/#LinuxSetup/StarterApps.sh
@@ -3,8 +3,9 @@ sudo apt-get install ncftp -y --force-yes
 sudo apt-get install p7zip-full -y --force-yes
 sudo apt-get install cmake-qt-gui -y --force-yes
 sudo apt-get install filezilla -y --force-yes
-sudo apt-get install eclipse-cdt -y --force-yes
+#sudo apt-get install eclipse-cdt -y --force-yes
 sudo apt-get install svn-workbench -y --force-yes
 sudo apt-get install mesa-common-dev -y --force-yes
 sudo apt-get install libgl1-mesa-dev -y --force-yes
 sudo apt-get install libglu1-mesa-dev -y --force-yes
+sudo apt-get install g++ -y --force-yes
